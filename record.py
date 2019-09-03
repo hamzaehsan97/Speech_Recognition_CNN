4242b768077fd18e1daccca958fa3725fbecfcd2
Hamza Ehsan <hamzaehsan@Hamzas-MacBook-Pro.local>
2019-09-03 11:41:02 -0500

    Updated code

--- record.py
+++ record.py
@@ -16,36 +16,6 @@ import main as mn
 from keras.models import model_from_json
 import os
 
-
-
-json_file = open('model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-model = model_from_json(loaded_model_json)
-# load weights into new model
-model.load_weights("model.h5")
-model.compile(loss='categorical_crossentropy', optimizer='adam',
-                metrics=['accuracy'])
-
-
-   
-def predictRecording():
-        # Save data to array file first
-        save_data_to_array(max_len=11, n_mfcc=20)
-        x = np.load("prediction.npy")
-        x = x.reshape(x.shape[0], 20, 11, 1)
-        y_final_oneHotEncoded= model.predict_classes(x, batch_size=1, verbose=0)
-        y_final_prob= model.predict_proba(x, batch_size=len(x), verbose=0)
-        predictNumber = y_final_oneHotEncoded[0]
-        listNames = ["cat","bed","happy"]
-        print(f"PREDICTION NUMBER = {predictNumber}")
-        print(f"PREDICTION = {listNames[predictNumber]}")
-        print(f"PREDICTION Prob= {y_final_prob}")
-        start()
-        
-        
-
-
 def testRecord():
     print("Recording for one second")    
     fs = 44100  # Sample rate
@@ -56,7 +26,7 @@ def testRecord():
     write("output.wav", fs, myrecording)
     # os.rename changes file location to the right folder however, it tends to corrupt the .WAV file
     #os.rename('/Users/hamzaehsan/Desktop/titleTownTech/speechRecognition/output.wav', '/Users/hamzaehsan/Desktop/titleTownTech/speechRecognition/Predict/Prediction/output.wav')
-    predictRecording()
+    print("output.wav has been saved to project directory")
 
 def start():
         print("PRESS RETURN TO RECORD")
